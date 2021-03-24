835136ce5a2c90309e73ec4bd4efc6c7766daa36
Pranay Narang <pranaynarang@gmail.com>
2021-03-24 15:52:00 +0530

    Add authorization middleware
    - Supports RBAC with 'role' claim from idToken
    - Supports basic authorization with just the idToken

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,35 @@
+/*
+* Firebase Authorization Middleware
+*
+* Example usage with basic verification:
+*         router.get('/', auth(), controller) 
+* 
+* Example usage with RBAC:
+*         router.get('/', auth({hasRole: ['admin', 'user']}), controller)          
+*/
+const admin = require("firebase-admin");
+
+module.exports = (options) => {
+    return async (req, res, next) => {
+        try {
+            const decodedToken = await admin
+                .auth()
+                .verifyIdToken(req.header('Authorization').replace('Bearer ', ''))
+
+            req.uid = decodedToken.uid
+
+            if (!options) {
+                return next()
+            }
+
+            var allow = options.hasRole.some(elem => elem == decodedToken.role)
+            if (!allow) {
+                return res.status(401).send({ error: 'Invalid role' })
+            }
+            next()
+
+        } catch (e) {
+            res.status(401).send({ error: 'Access denied' })
+        }
+    }
+}
