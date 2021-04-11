cf0be19a3618c8568ffbac903f8d33dbcaa3099e
sweekar52 <sweekarvb@gmail.com>
2021-04-11 13:22:37 +0530

    services: Add basic designer CRUD API

---
+++ routes/designer.route.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+
+const controller = require('../controllers/designer.controller')
+
+const auth = require('../middleware/auth.middleware')
+
+router.post('/designers', auth({ hasRole: ['owner'] }), controller.add)
+router.get('/designers', auth({ hasRole: ['owner'] }), controller.read)
+router.patch('/designers/:id', auth({ hasRole: ['owner'] }), controller.update)
+router.delete('/designers/:id', auth({ hasRole: ['owner'] }), controller.remove)
+
+module.exports = router
