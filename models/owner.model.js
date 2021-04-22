dd5141bf5b987a313e18a1c9c241b939cfe0266a
Pranay Narang <pranaynarang@gmail.com>
2021-04-22 18:04:55 +0530

    services: owners: Remove the phone field from model

--- models/owner.model.js
+++ models/owner.model.js
@@ -11,10 +11,6 @@ const schema = new mongoose.Schema({
         type: String,
         required: true
     },
-    phone: {
-        type: String,
-        required: true
-    },
     email: {
         type: String,
         required: true,
