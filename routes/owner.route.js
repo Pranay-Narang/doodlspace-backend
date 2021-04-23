34f4565311f17af1b8847538b2168ec08003c658
Pranay Narang <pranaynarang@gmail.com>
2021-04-23 19:33:42 +0530

    services: owners: Allow some operations only for superadmins

--- routes/owner.route.js
+++ routes/owner.route.js
@@ -4,9 +4,9 @@ const controller = require('../controllers/owner.controller')
 
 const auth = require('../middleware/auth.middleware')
 
-router.post('/owners', auth({ hasRole: ['owner'] }), controller.add)
-router.get('/owners', auth({ hasRole: ['owner'] }), controller.read)
-router.patch('/owners/:id', auth({ hasRole: ['owner'] }), controller.update)
-router.delete('/owners/:id', auth({ hasRole: ['owner'] }), controller.remove)
+router.post('/owners', auth({ hasRole: ['superadmin'] }), controller.add)
+router.get('/owners', auth({ hasRole: ['owner', 'superadmin'] }), controller.read)
+router.patch('/owners/:id', auth({ hasRole: ['superadmin'] }), controller.update)
+router.delete('/owners/:id', auth({ hasRole: ['superadmin'] }), controller.remove)
 
 module.exports = router
