ef1fd4e0912f9c4cb5c25e2d18cb212ed8fefe09
Pranay Narang <pranaynarang@gmail.com>
2021-06-02 15:20:55 +0530

    models: designrequests: Remove un-needed imports

--- models/designrequest.model.js
+++ models/designrequest.model.js
@@ -1,15 +1,5 @@
 const mongoose = require("mongoose")
 const validator = require("validator")
-const S3 = require('aws-sdk/clients/s3');
-
-const CONFIG = require('../config/config');
-
-const s3 = new S3({
-    accessKeyId: CONFIG.AWS_ACCESS_KEY_ID,
-    secretAccessKey: CONFIG.AWS_SECRET_ACCESS_KEY,
-    endpoint: CONFIG.AWS_S3_BUCKET_ENDPOINT,
-    s3ForcePathStyle: true
-});
 
 const schema = new mongoose.Schema({
     cid: String,
