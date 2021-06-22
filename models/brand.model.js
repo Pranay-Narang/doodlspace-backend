e251e91dd10bd294718dbaa01d6f7c2aa0782193
Pranay Narang <pranaynarang@gmail.com>
2021-06-22 13:49:30 +0530

    models: brand: Remove un-needed imports

--- models/brand.model.js
+++ models/brand.model.js
@@ -1,15 +1,5 @@
 const mongoose = require("mongoose")
 const validator = require("validator")
-const S3 = require('aws-sdk/clients/s3');
-
-const CONFIG = require('../config/config');
-
-const s3 = new S3({
-    accessKeyId: CONFIG.AWS_ACCESS_KEY_ID,
-    secretAccessKey: CONFIG.AWS_SECRET_ACCESS_KEY,
-    endpoint: CONFIG.AWS_S3_BUCKET_ENDPOINT,
-    s3ForcePathStyle: true
-});
 
 const socialSchema = new mongoose.Schema({
     name: String,
@@ -52,20 +42,6 @@ const schema = new mongoose.Schema(
         timestamps: true
     }
 )
-
-schema.methods.generatePreSignedURL = async function (subObject) {
-    const objects = this[subObject].toObject()
-
-    const objectURLs = objects.map(object => {
-        const params = {
-            Bucket: CONFIG.AWS_S3_BUCKET,
-            Key: object,
-            Expires: 60 * 5
-        };
-        return {name: object.split('\\').pop().split('/').pop(), link: s3.getSignedUrl('getObject', params)}
-    })
-    return objectURLs
-}
 
 const Brand = mongoose.model('Brand', schema)
 
