3db3076c9ea8d19acfd23f62daa0d5394b62c63e
Pranay Narang <pranaynarang@gmail.com>
2021-07-13 16:15:02 +0530

    routes: comments: Remove file filter

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -29,13 +29,13 @@ const upload = multer({
             cb(null, 'designrequests/uploads/' + req.uid + '/' + file.fieldname + '/' + file.originalname)
         }
     }),
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(png|jpg)$/)) {
-            return cb(new Error('Please upload a PNG/JPG'))
-        }
+    // fileFilter(req, file, cb) {
+    //     if (!file.originalname.match(/\.(png|jpg)$/)) {
+    //         return cb(new Error('Please upload a PNG/JPG'))
+    //     }
 
-        cb(undefined, true)
-    },
+    //     cb(undefined, true)
+    // },
 });
 
 router.post('/designrequests/:id/comments', auth({ hasRole: ['customer', 'supervisor', 'designer', 'owner'] }), upload.fields([
